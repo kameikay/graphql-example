27ff3d07658e902bb64bb455eb89298363b5a56d
Victor Kamei Kay <kameikay@gmail.com>
2024-01-03 12:17:50 -0300

    first commit

---
+++ cmd/server/server.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/kameikay/graphql-example/graph"
+	"github.com/kameikay/graphql-example/internal/database"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	db, err := sql.Open("sqlite3", "./database.sql")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	categoryDb := database.NewCategory(db)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		CategoryDB: categoryDb,
+	}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
