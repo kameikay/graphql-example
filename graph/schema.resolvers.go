854ed0d206d0857477c232140270632f5c2a8167
Victor Kamei Kay <kameikay@gmail.com>
2024-01-05 11:05:01 -0300

    separate models and create nested queries

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -6,10 +6,28 @@ package graph
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/kameikay/graphql-example/graph/model"
 )
+
+// Courses is the resolver for the courses field.
+func (r *categoryResolver) Courses(ctx context.Context, obj *model.Category) ([]*model.Course, error) {
+	courses, err := r.CourseDB.FindByCategoryID(obj.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	var gqlCourses []*model.Course
+	for _, course := range courses {
+		gqlCourses = append(gqlCourses, &model.Course{
+			ID:          course.ID,
+			Name:        course.Name,
+			Description: &course.Description,
+		})
+	}
+
+	return gqlCourses, nil
+}
 
 // CreateCategory is the resolver for the createCategory field.
 func (r *mutationResolver) CreateCategory(ctx context.Context, input model.NewCategory) (*model.Category, error) {
@@ -60,8 +78,25 @@ func (r *queryResolver) Categories(ctx context.Context) ([]*model.Category, erro
 
 // Courses is the resolver for the courses field.
 func (r *queryResolver) Courses(ctx context.Context) ([]*model.Course, error) {
-	panic(fmt.Errorf("not implemented: Courses - courses"))
+	courses, err := r.CourseDB.FindAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var gqlCourses []*model.Course
+	for _, course := range courses {
+		gqlCourses = append(gqlCourses, &model.Course{
+			ID:          course.ID,
+			Name:        course.Name,
+			Description: &course.Description,
+		})
+	}
+
+	return gqlCourses, nil
 }
+
+// Category returns CategoryResolver implementation.
+func (r *Resolver) Category() CategoryResolver { return &categoryResolver{r} }
 
 // Mutation returns MutationResolver implementation.
 func (r *Resolver) Mutation() MutationResolver { return &mutationResolver{r} }
@@ -69,5 +104,6 @@ func (r *Resolver) Mutation() MutationResolver { return &mutationResolver{r} }
 // Query returns QueryResolver implementation.
 func (r *Resolver) Query() QueryResolver { return &queryResolver{r} }
 
+type categoryResolver struct{ *Resolver }
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
--- internal/database/course.go
+++ internal/database/course.go
@@ -54,3 +54,25 @@ func (c *Course) FindAll() ([]*Course, error) {
 
 	return courses, nil
 }
+
+func (c *Course) FindByCategoryID(categoryID string) ([]*Course, error) {
+	rows, err := c.db.Query("SELECT id, name, description, category_id FROM courses WHERE category_id = $1", categoryID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var courses []*Course
+
+	for rows.Next() {
+		var course Course
+		err := rows.Scan(&course.ID, &course.Name, &course.Description, &course.CategoryID)
+		if err != nil {
+			return nil, err
+		}
+
+		courses = append(courses, &course)
+	}
+
+	return courses, nil
+}
